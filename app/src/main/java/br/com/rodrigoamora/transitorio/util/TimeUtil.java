f9ec9908c20a2d8876edbe8fe07a98089cdd6125
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2018-07-31 14:58:48 -0300

    Ajustes

--- app/src/main/java/br/com/rodrigoamora/transitorio/util/TimeUtil.java
+++ app/src/main/java/br/com/rodrigoamora/transitorio/util/TimeUtil.java
@@ -5,14 +5,14 @@ import java.util.Date;
 public class TimeUtil {
 
     public static double calculateTimeInMinutes(String hour) {
-        Date date = new Date();
-        date.setHours(Integer.valueOf(hour.split(":")[0]));
-        date.setMinutes(Integer.valueOf(hour.split(":")[1]));
-        date.setSeconds(Integer.valueOf(hour.split(":")[2]));
+        Date consultedHour = new Date();
+        consultedHour.setHours(Integer.valueOf(hour.split(":")[0]));
+        consultedHour.setMinutes(Integer.valueOf(hour.split(":")[1]));
+        consultedHour.setSeconds(Integer.valueOf(hour.split(":")[2]));
 
-        Date date2 = new Date();
+        Date currentHour = new Date();
 
-        long difference = date2.getTime() - date.getTime();
+        long difference = currentHour.getTime() - consultedHour.getTime();
         double differenceInMinutes = (difference /1000) / 60;
 
         return differenceInMinutes;
