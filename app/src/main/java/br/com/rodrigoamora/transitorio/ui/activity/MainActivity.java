378f99d6267af833b744ccb25a40298eab4afc84
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2019-10-14 00:36:51 -0300

    Commenting the method navigationBarTransparent()

--- app/src/main/java/br/com/rodrigoamora/transitorio/ui/activity/MainActivity.java
+++ app/src/main/java/br/com/rodrigoamora/transitorio/ui/activity/MainActivity.java
@@ -27,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         checkPermissions();
         hideNavigationBar();
-        navigationBarTransparent();
+//        navigationBarTransparent();
         changeFragment(new OnibusFragment(), null);
     }
 
